913a7616127171af85cd915150343a14ea97cb4e
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-10-31 01:10:32 +0900

    feat: useEffect를 통해 api 연결

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,12 +1,53 @@
 // Movies.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Movie from "./Movie";
-import { MOVIES } from "../movies"; // MOVIES 데이터를 가져옵니다
+
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface MoviesProps {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 const Movies: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const movies = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: "Bearer token",
+          },
+        }
+      );
+      setMovies(movies.data.results);
+    };
+    getMovies();
+  }, []);
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-4">
-      {MOVIES.results.map((movie) => (
+      {movies?.map((movie) => (
         <Movie key={movie.id} img={movie.poster_path} /> // Movie 컴포넌트에 poster_path를 전달
       ))}
     </div>
