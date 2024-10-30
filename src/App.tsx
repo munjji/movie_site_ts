40244a6895db9b9eb406aa7882a37286554eaae9
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-10-31 00:18:39 +0900

    core: router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Movies from "./components/Movies";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>홈 페이지입니다.</h1>,
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Movies />
+      <RouterProvider router={router} />
     </>
   );
 }
