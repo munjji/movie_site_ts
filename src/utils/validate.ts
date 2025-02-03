395f19f1610f5cd4a16b47dee914a09bcdb0d318
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-02-03 18:01:32 +0900

    refactor: zod 사용해서 유효성 검사 수정

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,20 +1,36 @@
-const emailPattern: RegExp =
-  /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}$/;
+import { z } from "zod";
 
-interface LoginValues {
-  email: string;
-  password: string;
-}
+// 이메일 유효성 검사
+const emailSchema = z.string().email("올바른 이메일 형식이 아닙니다.");
+
+// 비밀번호 유효성 검사
+const passwordSchema = z
+  .string()
+  .min(8, "비밀번호는 8자 이상이어야 합니다.")
+  .max(16, "비밀번호는 16자 이하로 입력해주세요.");
+
+// 로그인 유효성 검사
+const loginSchema = z.object({ email: emailSchema, password: passwordSchema });
+
+// 회원가입 유효성 검사
+const signupSchema = z
+  .object({
+    email: emailSchema,
+    password: passwordSchema,
+    repassword: z.string(),
+  })
+  .refine((data) => data.password === data.repassword, {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["repassword"],
+  });
+
+// 로그인, 회원가입 values 정의
+type LoginValues = z.infer<typeof loginSchema>;
+type SignupValues = z.infer<typeof signupSchema>;
 
 interface ValidationErrors {
   email?: string;
   password?: string;
-}
-
-interface SignupValues {
-  email: string;
-  password: string;
-  repassword: string;
 }
 
 interface ValidationSignupErrors {
@@ -23,40 +39,31 @@ interface ValidationSignupErrors {
   repassword?: string;
 }
 
-// ✅ 로그인 유효성 검사 함수
+// 로그인 유효성 검사 함수
 function validateLogin(values: LoginValues): ValidationErrors {
-  const errors: ValidationErrors = {};
+  const result = loginSchema.safeParse(values);
+  if (result.success) return {};
 
-  if (!emailPattern.test(values.email)) {
-    errors.email = "올바른 이메일 형식이 아닙니다.";
-  }
-
-  if (values.password.length < 8 || values.password.length > 16) {
-    errors.password = "비밀번호는 8~16자 사이로 입력해주세요.";
-  }
-
-  return errors;
+  // `flatten().fieldErrors`의 타입을 변환
+  const errors = result.error.flatten().fieldErrors;
+  return {
+    email: errors.email?.[0],
+    password: errors.password?.[0],
+  };
 }
 
-// ✅ 회원가입 유효성 검사 함수 (비밀번호 확인 포함)
+// 회원가입 유효성 검사 함수 (비밀번호 확인 포함)
 function validateSignup(values: SignupValues): ValidationSignupErrors {
-  const errors: ValidationSignupErrors = {};
+  const result = signupSchema.safeParse(values);
+  if (result.success) return {};
 
-  if (!emailPattern.test(values.email)) {
-    errors.email = "이메일을 반드시 입력해주세요.";
-  }
-
-  if (values.password.length < 8 || values.password.length > 16) {
-    errors.password = "비밀번호는 8~16자 사이로 입력해주세요.";
-  }
-
-  if (!values.repassword) {
-    errors.repassword = "비밀번호 확인을 입력해주세요.";
-  } else if (values.repassword !== values.password) {
-    errors.repassword = "비밀번호가 일치하지 않습니다.";
-  }
-
-  return errors;
+  // `flatten().fieldErrors`의 타입을 변환
+  const errors = result.error.flatten().fieldErrors;
+  return {
+    email: errors.email?.[0],
+    password: errors.password?.[0],
+    repassword: errors.repassword?.[0],
+  };
 }
 
 export { validateLogin, validateSignup };
